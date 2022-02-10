4bd767211c0d34b4381de0c40a91cd78f8470c76
ddLaptopWindows <ddforwork08@gmail.com>
2022-02-10 17:56:34 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,7 +7,9 @@ const main = async () => {
 
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
+    await waveContract.deployed({
+        value: hre.ethers.utils.parseEther("0.01"),
+    });
 
     console.log("WavePortal address: ", waveContract.address);
 };
